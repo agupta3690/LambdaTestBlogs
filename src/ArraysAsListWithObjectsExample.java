ff7be38bfa6d1a2368330272b2d4145c7a45a93e
agupta3690 <arun.gupta36@yahoo.com>
2023-04-23 01:17:52 +0530

    Update ArraysAsListWithObjectsExample.java

--- src/ArraysAsListWithObjectsExample.java
+++ src/ArraysAsListWithObjectsExample.java
@@ -1,26 +1,36 @@
 import java.util.Arrays;
 import java.util.List;
 
+
 public class ArraysAsListWithObjectsExample {
 
 
-	public static void main (String args[]) {
 
-		//Getting the list view of ProgrammingLanguage class objects
+   public static void main (String args[]) {
 
-		List <ProgrammingLanguage> list = Arrays.asList(
 
-				new ProgrammingLanguage("Java", 17.0),
-				new ProgrammingLanguage("Python", 3.10),
-				new ProgrammingLanguage("C", 17.0),
-				new ProgrammingLanguage("Ruby", 3.2)
+       //Getting the list view of ProgrammingLanguage class objects
 
-				);
 
-		//Printing the objects inside the list
+       List <ProgrammingLanguage> list = Arrays.asList(
 
-		System.out.println(list);
 
-	}
+               new ProgrammingLanguage("Java", 17.0),
+               new ProgrammingLanguage("Python", 3.10),
+               new ProgrammingLanguage("C", 17.0),
+               new ProgrammingLanguage("Ruby", 3.2)
+
+
+               );
+
+
+       //Printing the objects inside the list
+
+
+       System.out.println(list);
+
+
+   }
+
 
 }
