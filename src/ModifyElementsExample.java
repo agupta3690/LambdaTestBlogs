4ff0a2c1927f7ed20236b417d02eecb844d299b8
agupta3690 <arun.gupta36@yahoo.com>
2023-04-23 01:32:39 +0530

    Update ModifyElementsExample.java

--- src/ModifyElementsExample.java
+++ src/ModifyElementsExample.java
@@ -1,28 +1,28 @@
 import java.util.Arrays;
 import java.util.List;
 
-public class ModifyElementsExample {
-	
-	public static void main (String args[]) {
-		
-		
 
-		//Initializing a string Array
-		
-		String [] arr = {"Java", "Python", "C", "Ruby"};
-		
-		//Getting the list view of Array
-		
-		List <String> list = Arrays.asList(arr);
-		
-		//Modifying the list
-		
-		list.set(3, "C++");
-		
-		//Printing the original Array
-		
-		System.out.println(Arrays.toString(arr));
-		
-	}
+public class ModifyElementsExample {
+  
+   public static void main (String args[]) {
+      
+       //Initializing a string Array
+      
+       String [] arr = {"Java", "Python", "C", "Ruby"};
+      
+       //Getting the list view of Array
+      
+       List <String> list = Arrays.asList(arr);
+      
+       //Modifying the value of the list
+      
+       list.set(3, "C++");
+      
+       //Printing the original Array
+      
+       System.out.println(Arrays.toString(arr));
+      
+   }
+
 
 }
