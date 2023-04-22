9c2d8e324d0884198482a7f11ce76b661da55308
agupta3690 <arun.gupta36@yahoo.com>
2023-04-23 01:17:07 +0530

    Update ProgrammingLanguage.java

--- src/ProgrammingLanguage.java
+++ src/ProgrammingLanguage.java
@@ -1,27 +1,38 @@
 public class ProgrammingLanguage{
 
-	//Declaring the instance variables
 
-	public String name;
-	public double version;
-
-	//Constructor to initialize the instance variables
-
-	public ProgrammingLanguage(String name, double version) {
-
-		this.name = name;
-		this.version = version;
+   //Declaring the instance variables
 
 
-	}
+   public String name;
+   public double version;
 
-	// Overriding toString method to return the String representation of the object
 
-	public String toString() {
+   //Constructor to initialize the instance variables
 
-		return "(" + this.name + "," + this.version + ")";
 
-	}
+   public ProgrammingLanguage(String name, double version) {
+
+
+       this.name = name;
+       this.version = version;
+
+
+   }
+
+
+   // Overriding toString method to return the String representation of the object
+
+
+   public String toString() {
+
+
+       return "(" + this.name + "," + this.version + ")";
+
+
+   }
+
+
 
 
 }
